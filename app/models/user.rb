7e28bf23f9a43b3592819e9cf0d60df8a6cd54ea
James <romanchukenator@gmail.com>
2014-08-10 22:44:40 -0700

    Secure password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
   before_save { self.email = email.downcase }
-## additional attributes: phone number, company name, address, first name, last name
+
+  ## additional attributes: phone number, company name, address, first name, last name
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -8,7 +10,7 @@ class User < ActiveRecord::Base
     presence: true, 
     format: { with: VALID_EMAIL_REGEX }, 
     uniqueness: { case_sensitive: false }
-  
+
   validates :password, 
     presence: true, 
     length: { minimum: 4 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811024511) do
+ActiveRecord::Schema.define(version: 20140811052554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
