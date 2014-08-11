7de17100b3376f631a9d7ada82c2225217314aa5
James <romanchukenator@gmail.com>
2014-08-11 11:01:36 -0700

    Mail templates updated, default email addresses updated

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,42 +1,38 @@
 class ReportMailer < ActionMailer::Base
-  default from: 'rahul@getdashound.com'
+  default from: "rahul@getdashound.com"
+  default to: "rahul@getdashound.com"
 
   def self.call(user, reports = nil)
-    report_mail = welcome(user, reports)
+    welcome_mail = welcome(user)
+    report_mail = reports(user, reports)
+    
+    welcome_mail.deliver
     report_mail.deliver
   end
 
-  def welcome(user, reports)
+  def welcome(user)
     @user = user
-    @url  = 'http://beta.getdashound.com'
-    @reports = Hash.new
+    @url  = "http://beta.getdashound.com"
 
-    @reports[:profit_loss] = reports[:file_pl].tempfile
-    @reports[:balance_sheet] = reports[:file_bs].tempfile
-    @reports[:cash_flow] = reports[:file_cf].tempfile
-
-    attachments['profitLoss.csv'] = File.read(@reports[:profit_loss])
-    attachments['balanceSheet.csv'] = File.read(@reports[:balance_sheet])
-    attachments['cashFlow.csv'] = File.read(@reports[:cash_flow])
-
-    mail(to: @user.email, subject: 'You got Dashound. Sweet.')
+    mail(to: "#{@user.email}", 
+        subject: "Your benchmarks are coming soon, no worries.")
   end
 
   def reports(user, reports)
     @user = user
-    @url  = 'http://beta.getdashound.com'
+    @url  = "http://beta.getdashound.com"
     @reports = Hash.new
+    @company_name = reports["company_name"]
 
     @reports[:profit_loss] = reports[:file_pl].tempfile
     @reports[:balance_sheet] = reports[:file_bs].tempfile
     @reports[:cash_flow] = reports[:file_cf].tempfile
 
-    attachments['profitLoss.csv'] = File.read(@reports[:profit_loss])
-    attachments['balanceSheet.csv'] = File.read(@reports[:balance_sheet])
-    attachments['cashFlow.csv'] = File.read(@reports[:cash_flow])
+    attachments["profitLoss.csv"] = File.read(@reports[:profit_loss])
+    attachments["balanceSheet.csv"] = File.read(@reports[:balance_sheet])
+    attachments["cashFlow.csv"] = File.read(@reports[:cash_flow])
 
-    mail(to: "rahul@getdashound.com", 
-        from: @user.email, 
-        subject: 'Yo, @user.email from @user.company wants you to rip through these reports!')
+    mail(from: @user.email, 
+        subject: "Yo, dude at #{@user.email} wants you to rip through these reports for #{@company_name}!")
   end
 end
